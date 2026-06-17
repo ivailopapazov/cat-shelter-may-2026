d87868d66753095945b8a8cf96bc988f04ad81a5
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2026-06-17 20:23:45 +0300

    Add dynamic breed options in add cat page

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ const server = http.createServer(async (req, res) => {
             htmlContent = await fs.readFile('./src/views/addBreed.html', 'utf-8');
             break;
         case '/cats/add-cat':
-            htmlContent = await fs.readFile('./src/views/addCat.html', 'utf-8');
+            htmlContent = await renderAddCatPage();
             break;
         default:
             htmlContent = await fs.readFile('./src/views/notFound.html', 'utf-8');
@@ -87,6 +87,15 @@ async function renderHomePage() {
 
     const catsContent = `<ul>${cats.map(cat => catTemplate(cat)).join('\n')}</ul>`;
     const result = htmlContent.replace('{{cats}}', catsContent);
+
+    return result;
+}
+
+async function renderAddCatPage() {
+    const htmlContent = await fs.readFile('./src/views/addCat.html', 'utf-8');
+
+    const breedOptions = readBreeds().map(breed => `<option value="${breed.id}">${breed.name}</option>`).join('\n');
+    const result = htmlContent.replace('{{breadOptions}}', breedOptions);
 
     return result;
 }
