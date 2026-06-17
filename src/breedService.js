8e2c9bf96b464cbe5529fbcd9844354e87a14784
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2026-06-17 19:41:51 +0300

    Add breed dynamically

---
+++ src/breedService.js
@@ -0,0 +1,16 @@
+import breeds from './breeds.js';
+
+export function readBreeds() {
+    return breeds;
+}
+
+export function addBreed(breedName) {
+    const newBreed = {
+        id: breeds.length + 1,
+        name: breedName
+    };
+
+    breeds.push(newBreed);
+}
+
+
